e29e196e9100d0da9f55caad6c7546ea77291f72
Eric Tuvesson <eric.tuvesson@gmail.com>
2020-02-01 11:45:13 +0100

    Install AirConsole
    Followed the Tutorial on:
    https://www.youtube.com/watch?v=zPpGoJVA3Tw
    
    I don't go into the build system, which is something we have to look at later.

---
+++ GGJ20/Assets/GameLogic.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NDream.AirConsole;
+using Newtonsoft.Json.Linq;
+
+public class GameLogic : MonoBehaviour
+{
+    void Awake() {
+        AirConsole.instance.onMessage += onMessage;
+    }
+
+    void onMessage(int fromDeviceID, JToken data) {
+      Debug.Log("message from " + fromDeviceID + ", data: " + data);
+        
+    }
+
+    void OnDestroy() {
+      if (AirConsole.instance != null) {
+        AirConsole.instance.onMessage -= onMessage;
+      }
+    }
+}
